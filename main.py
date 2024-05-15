7882250165b4b5f0affac56af161b02441f1df10
Mayo, Sam <sdm2000@hw.ac.uk>
2024-05-15 18:50:53 +0100

    batman

---
+++ main.py
@@ -0,0 +1,113 @@
+import glob
+import json
+from xml_handler import dict_to_xml, xml_to_string, pretty_print_xml, save_pretty_xml_to_file, read_xml
+
+
+def unit_artdef(folder, config):
+    artdef_units = config['units_specified']
+    artdef_name = 'Units.artdef'
+    artdefs = [f for f in glob.glob(f'{folder}/**/*{artdef_name}*', recursive=True)]
+    if len(artdefs) == 0:
+        raise FileNotFoundError(f'No files were found that match Units.artdef. Make sure your config.json points to your civ VI directory.')
+
+    artdef_info = read_xml(artdefs[0])
+    artdef_info_ = artdef_info['AssetObjects..ArtDefSet']['m_RootCollections']['Element']
+    full_artdef = {i['m_CollectionName']['@text']: i.get('Element', []) for i in artdef_info_}
+
+    for artdef in artdefs[1:]:
+        artdef_info = read_xml(artdef)
+        artdef_info_ = artdef_info['AssetObjects..ArtDefSet']['m_RootCollections']['Element']
+        artdef_dict = {i['m_CollectionName']['@text']: i['Element'] for i in artdef_info_ if
+                       i.get('Element', None) is not None}
+
+        for i, j in artdef_dict.items():
+            if isinstance(j, dict):
+                full_artdef[i].append(j)
+            elif isinstance(j, list):
+                full_artdef[i].extend(j)
+            else:
+                print('weird collection')
+
+    uniques = []
+    not_uniques = []
+    drop_indices = []
+    for idx, i in enumerate(full_artdef['Units']):
+        if i['m_Name']['@text'] not in uniques:
+            uniques.append(i['m_Name']['@text'])
+        else:
+            not_uniques.append(i)
+            drop_indices.append(idx)
+
+    drop_indices.reverse()
+    for i in drop_indices:
+        del full_artdef['Units'][i]
+
+    artdef_total = {i['m_Name']['@text']: i for i in full_artdef['Units']}
+    # reset scout, warrior, due to the scout cat pack, and the zombies pack
+    artdef_total['UNIT_SCOUT'] = [i for i in not_uniques if 'SCOUT' in i['m_Name']['@text']][0]
+    artdef_total['UNIT_WARRIOR'] = [i for i in not_uniques if 'WARRIOR' in i['m_Name']['@text']][0]
+    artdef_template = read_xml('Units_template.artdef')
+    artdef_template['AssetObjects']['m_RootCollections']['Element']['Element'] = []
+    root = artdef_template['AssetObjects']['m_RootCollections']['Element']['Element']
+
+    failed = {'multsearch': [], 'nosearch': []}
+    search_found = []
+    used = []
+    for mod_ref, vanilla_ref in artdef_units.items():
+        if artdef_total.get(vanilla_ref) is None:
+            search = [i for i in artdef_total if vanilla_ref.replace('UNIT_', '') in i]
+            if len(search) > 1:
+                root.append(assign_artdef(artdef_total[search[0]], mod_ref))
+                print(f'found more than one match for {vanilla_ref}, using the first found.')
+                failed['multsearch'].append(artdef_units[mod_ref])
+                used.append(vanilla_ref)
+            elif len(search) == 0:
+                print(f'no match for {vanilla_ref}')
+                failed['nosearch'].append(artdef_units[mod_ref])
+            else:
+                root.append(assign_artdef(artdef_total[search[0]], mod_ref))
+                search_found.append(artdef_units[mod_ref])
+                used.append(vanilla_ref)
+                print(f'Found partial match for specified {vanilla_ref}. {mod_ref} now uses {search[0]}')
+        else:
+            root.append(assign_artdef(artdef_total[vanilla_ref], mod_ref))
+            used.append(vanilla_ref)
+            print(f"Success! {mod_ref} now uses {artdef_total[vanilla_ref]['m_Name']['@text']}")
+
+    # remake the weird formation
+    #artdef_template['AssetObjects::ArtDefSet'] = artdef_template.pop('AssetObjects')
+    root = dict_to_xml(artdef_template)
+    xml_string = xml_to_string(root)
+    pretty_xml_string = pretty_print_xml(xml_string)
+    pretty_xml_string = pretty_xml_string.replace('AssetObjects', 'AssetObjects::ArtDefSet')
+    save_pretty_xml_to_file(pretty_xml_string, artdef_name)
+
+    print(f'Saved unit mapping to {artdef_name}. Import it into modbuddy and you should be able to build with it.')
+    if config["show_all_possible_units"]:
+        total_units = set([i for i in artdef_total])
+        unused = list(total_units - set(used))
+        print('Here are all the other units you could have used (although some are scenario only):')
+        print(f"{unused}")
+    else:
+        print('If you want to see the list of units you could use, set config.jsons "show_all_possible_units" to true.')
+
+
+def assign_artdef(artdef, name):
+    artdef_cpy = artdef.copy()
+    artdef_cpy['m_Name'] = {'@text': name}
+    return artdef_cpy
+
+
+def main():
+    with open("config.json", 'r') as json_file:
+        config = json.load(json_file)
+    folder = config.get('civ_install', None)
+    if folder == "YOUR_DIRECTORY_HERE":
+        raise FileNotFoundError(
+            "Set your civ VI install filepath in config.json.")
+
+    unit_artdef(folder, config)
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,18 @@
+The purpose of this tool is to allow quick and easy use of pre-existing Civ VI models in your mod.
+
+It does this by searching through your Civ VI install, absorbing any Units.artdef file.
+
+To use this tool, you will need to edit the config.json file and change the value denoted by "civ_install" to your
+civ install directory, for example "C:/Steam/Steamapps/common/Sid Meier's Civilization VI". You need double quotes around
+I signified there.
+
+You will also need to specify your new units name, and what unit it should copy in the same file.
+Under units_specified is a list where you can set what your new unit on the left should be assigned to the civ unit on the right.
+
+You can also have a look at the full list of units available to make your unit look like by setting "show_all_possible_units" to True.
+
+Once the script has run, you should have a new file called Units.artdef. You can then import that into your modbuddy project to build
+the .dep file for your mod.
+
+Note: there are some scenario-only units that are importable, but will not show up in game, as the assets for them
+are not loaded.
